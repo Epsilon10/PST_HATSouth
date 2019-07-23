fa4f4f559a13925591b499ae7d09ab9bccb62e12
Epsilon10 <benjamin.oommen1@gmail.com>
2019-07-23 16:11:56 -0500

    add latex doc

--- scripts/src/spl_coeff_solver.py
+++ scripts/src/spl_coeff_solver.py
@@ -6,6 +6,17 @@ from dll_loader import PSFDllLoader
 
 
 class SubpixelCoeffSolver:
+    def __init__(self):
+        self.c_src = PSFDllLoader("./libpsf.so")
+    
+    
+    def _fill_sum_row(self, pixel_x, pixel_y, row):
+        for nx in range(self.subpixel_res_x):
+            for ny in range(self.subpixel_res_y):
+                x = pixel_x + nx*self.dx + self.dx/2.0 - self.x_0
+                y = pixel_y + ny*self.dy + self.dy/2.0 - self.y_0
+                print(x,y)
+                self.sums[row][ny + nx * self.subpixel_res_y] = self.interp((self.pixel_width * abs(x),self.pixel_height * abs(y)))
     """
     Solves for the coeffecients of the subpixel sensitivity map.
     Args:
@@ -13,38 +24,24 @@ class SubpixelCoeffSolver:
         - focal_ratio 
         - variance_x: standard deviation of the x coordinates of the spot centers
         - variance_y: standard deviation of the y coordinates of the spot centers
-        - num_subpxl_x: number of subpixels to split each pixel along the x axis
-        - num_subpxl_y: number of subpixels to split each pixel along the y axis
-        - subpxl_res_x: resolution of the ifft along the x axis
-        - subpxl_res_y: resolution of the ifft along the y axis
+        - subpixel_res_x: number of subpixels to split each pixel along the x axis
+        - subpixel_res_y: number of subpixels to split each pixel along the y axis
+        - map_res_x: resolution of the ifft along the x axis
+        - map_res_y: resolution of the ifft along the y axis
+        - max_x_wavenumber: boundary of the fourier transform along x
+        - max_y_wavenumber: boundary of the fourier transform along y
         - x_0: x coordinate of the spot center
         - y_0: y coordinate of the spot center
         - x_min: lower x coordinate of pixel
         - x_max: upper x coordinate of pixel
         - y_min: lower y coordinate of pixel
         - y_max: upper y coordinate of pixel
+        - pixel_width: width of pixel
+        - pixel_height: height of pixel
+        - respones: row vector of respones for all pixels
     """
-    def __init__(self):
-        self.c_src = PSFDllLoader("./libpsf.so")
-    
-    """
-    Allocates memory for matrices
-    Args:
-        - max_x_wavenumber
-        - max_y_wavenumber
-    """
-    
-    def _get_response(self, pixel_x, pixel_y, row):
-        for nx in range(self.subpixel_res_x):
-            for ny in range(self.subpixel_res_y):
-                x = pixel_x + nx*self.dx + self.dx/2.0 - self.x_0
-                y = pixel_y + ny*self.dy + self.dy/2.0 - self.y_0
-                print(x,y)
-                self.sums[row][ny + nx * self.subpixel_res_y] = self.interp((abs(x),abs(y)))
-        return np.sum(self.sums[row])
-    
     def solve(self, wavelength, focal_ratio, variance_x,variance_y, subpixel_res_x, subpixel_res_y, 
-        map_res_x, map_res_y, max_x_wavenumber, max_y_wavenumber, x_0, y_0, x_min, x_max, y_min, y_max):
+        map_res_x, map_res_y, max_x_wavenumber, max_y_wavenumber, x_0, y_0, x_min, x_max, y_min, y_max,pixel_width, pixel_height, responses):
         self.c_src.execute(
             wavelength, focal_ratio, variance_x,variance_y, subpixel_res_x, subpixel_res_y, 
             map_res_x, map_res_y, max_x_wavenumber, max_y_wavenumber
@@ -55,17 +52,23 @@ class SubpixelCoeffSolver:
         self.subpixel_res_x = subpixel_res_x
         self.subpixel_res_y = subpixel_res_y
 
-        self.dx = 1 / subpixel_res_x
-        self.dy = 1/subpixel_res_y
+        self.dx = 1.0 / subpixel_res_x
+        self.dy = 1.0 /subpixel_res_y
+
         self.x_0 = x_0
         self.y_0 = y_0
-        self.responses = np.empty(shape=num_x_pixels * num_y_pixels, dtype = c_double)
+
+        self.pixel_width = pixel_width
+        self.pixel_height = pixel_height
+
+        self.responses = responses
         self.sums = np.empty(shape = (num_x_pixels * num_y_pixels, subpixel_res_x * subpixel_res_y), dtype=c_double)
         self.interp = RegularGridInterpolator((self.c_src.x_vals,self.c_src.y_vals), self.c_src.I_vals)
+
         for pixel_x in range(x_min, x_max):
             for pixel_y in range(y_min, y_max):
                 row = pixel_y + pixel_x * num_y_pixels
-                self.responses[row] = self._get_response(pixel_x, pixel_y, row)
+                self._fill_sum_row(pixel_x, pixel_y, row)
         return np.linalg.lstsq(self.sums, self.responses, rcond = None)
 
 solver = SubpixelCoeffSolver()
