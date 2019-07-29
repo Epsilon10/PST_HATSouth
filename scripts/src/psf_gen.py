f46c4e07152c5038589605c6a69d5787c357bf94
Epsilon10 <benjamin.oommen1@gmail.com>
2019-07-29 13:20:11 -0500

    fix typo in math

--- scripts/src/psf_gen.py
+++ scripts/src/psf_gen.py
@@ -22,7 +22,10 @@ X, Y = np.meshgrid(c_src.kx_vals, c_src.ky_vals)
 
 Z = c_src.psi_amps
 
-surf = ax.plot_surface(X[:10,:10], Y[:10,:10], Z.transpose()[:10,:10], cmap=cm.coolwarm,
+ax.set_xlabel('x wavelengths')
+ax.set_ylabel('y wavelengths')
+ax.set_zlabel('PSF')
+surf = ax.plot_surface(X[:240,:240], Y[:240,:240], Z.transpose()[:240,:240], cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.savefig('plots/psf')
